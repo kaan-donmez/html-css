139aa2a38c15cf8609ae1146fd478347af8972a1
Kaan Donmez <kndonmezz@gmail.com>
2023-10-01 20:17:13 +0300

    Rotating Project was added

---
+++ CSS/Rotating/script.js
@@ -0,0 +1,11 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+
+open.addEventListener("click", () => {
+  container.classList.add("shownav");
+});
+
+close.addEventListener("click", () => {
+  container.classList.remove("shownav");
+});
